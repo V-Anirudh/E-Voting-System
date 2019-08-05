28bc22174dd56a28afc1a0d4004a1e005e5041f8
V-Anirudh <vallampatlaanirudh@gmail.com>
2019-08-05 11:29:57 +0530

    Create DBUtil.java

---
+++ src/com/wipro/evs/Util/DBUtil.java
@@ -0,0 +1,17 @@
+package com.wipro.evs.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+	public static Connection getDBConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "hr", "hr");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return connection;
+	}
+}
